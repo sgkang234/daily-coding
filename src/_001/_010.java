a0433905e77b5a6d7c2fbbf13f06d6b4e57e00f5
sgkang234 <sgkang234@gmail.com>
2026-03-16 01:56:47 +0900

    백준 11382

---
+++ src/_001/_010.java
@@ -0,0 +1,20 @@
+package _001;
+
+// 26.03.16
+// 백준 11382
+// A+B+C를 출력
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _010 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        long a = Integer.parseInt(input[0]);
+        long b = Integer.parseInt(input[1]);
+        long c = Integer.parseInt(input[2]);
+        System.out.println(a+b+c);
+    }
+}
