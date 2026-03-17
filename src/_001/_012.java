17215d8d58b0a9ea9759bcecb3cabdff1a8fcac6
sgkang234 <sgkang234@gmail.com>
2026-03-17 12:54:31 +0900

    백준 10172

---
+++ src/_001/_012.java
@@ -0,0 +1,15 @@
+package _001;
+
+// 26.03.17
+// 백준 10172
+// 강아지 출력
+
+public class _012 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
