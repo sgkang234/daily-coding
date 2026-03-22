f29a1b03eab1384247436174903bb32776af5eda
sgkang234 <sgkang234@gmail.com>
2026-03-22 20:08:06 +0900

    _10926_Surprised

--- src/Implementation/_10926_Surprised.java
+++ src/Implementation/_10926_Surprised.java
@@ -1,14 +1,14 @@
-package _001;
+package Implementation;
 
 // 26.03.15
 // 백준 10926번
-// 입력한 문자에 ??!를 붙여 출력
+// 입력한 문자에 "??!"를 붙여 출력하는 프로그램
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class _006 {
+public class _10926_Surprised {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
