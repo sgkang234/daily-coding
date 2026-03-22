5ae580a8e49d626e497382e6f86afebb00eee993
sgkang234 <sgkang234@gmail.com>
2026-03-22 21:28:18 +0900

    _25314_LongInt

--- src/Implementation/_25314_LongInt.java
+++ src/Implementation/_25314_LongInt.java
@@ -1,4 +1,4 @@
-package _001;
+package Implementation;
 
 // 2026.03.20
 // 백준 25314
@@ -8,7 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class _024 {
+public class _25314_LongInt {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
