997918ea1c59a8c6c52cc5a2bf474a3ea9ae40e3
sgkang234 <sgkang234@gmail.com>
2026-03-22 20:11:51 +0900

    _18108_불기연도

--- src/Implementation/_18108_불기연도.java
+++ src/Implementation/_18108_불기연도.java
@@ -1,4 +1,4 @@
-package _001;
+package Implementation;
 
 // 26.03.16
 // 백준 18108번
@@ -8,7 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class _007 {
+public class _18108_불기연도 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int year = Integer.parseInt(br.readLine());
