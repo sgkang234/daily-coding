45967ce3c0220602bc3c2405fde641d884c610dc
sgkang234 <sgkang234@gmail.com>
2026-03-22 19:54:21 +0900

    _10998_AMultiplyB

--- src/Implementation/_10998_AMultiplyB.java
+++ src/Implementation/_10998_AMultiplyB.java
@@ -1,14 +1,14 @@
-package _001;
+package Implementation;
 
 // 26.03.14
 // 백준 10998번
-// 두 정수 A와 B를 입력 받은 다음, A-B를 출력
+// 두 정수 A와 B를 입력 받은 다음, A*B를 출력
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class _004 {
+public class _10998_AMultiplyB {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" "); // A B로 입력 받은 것을 문자열 공백으로 나눈다.
