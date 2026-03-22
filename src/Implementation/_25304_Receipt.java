2ffd704f5329c25c9ff45e55675d6bf93a81eab4
sgkang234 <sgkang234@gmail.com>
2026-03-22 21:26:36 +0900

    _25304_Receipt

--- src/Implementation/_25304_Receipt.java
+++ src/Implementation/_25304_Receipt.java
@@ -1,4 +1,4 @@
-package _001;
+package Implementation;
 
 // 2026.03.20
 // 백준 25304
@@ -8,7 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class _023 {
+public class _25304_Receipt {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int x = Integer.parseInt(br.readLine());
