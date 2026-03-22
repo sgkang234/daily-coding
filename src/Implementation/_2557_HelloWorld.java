393ebe35e1a1563aeed6c0728a2222024b7ee0d4
sgkang234 <sgkang234@gmail.com>
2026-03-22 19:46:06 +0900

    2557_HelloWorld

--- src/Implementation/_2557_HelloWorld.java
+++ src/Implementation/_2557_HelloWorld.java
@@ -8,7 +8,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
-public class _2557 {
+public class _2557_HelloWorld {
     public static void main(String[] args) throws IOException {
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
