84bd9d1dd16f3b76c6528929d6f1feb27c946464
sgkang234 <sgkang234@gmail.com>
2026-03-22 19:49:07 +0900

    1000_APlusB

--- src/Implementation/_1000_APlusB.java
+++ src/Implementation/_1000_APlusB.java
@@ -1,15 +1,14 @@
-package _001;
+package Implementation;
 
 // 26.03.12
 // 백준 1000번
-// 두 정수 A와 B를 입력 받은 다음, A+B를 출력
+// 두 정수 A와 B를 입력 받은 다음, A+B를 출력하는 프로그램
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
-public class _002 {
+public class _1000_APlusB {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" "); // A B로 입력 받은 것을 문자열 공백으로 나눈다.
