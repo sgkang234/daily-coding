7c6f3511c3be071f330dfbab1221d80e758960dd
sgkang234 <sgkang234@gmail.com>
2026-03-22 20:24:43 +0900

    _1330_CompareTwoNumbers

--- src/Implementation/_1330_CompareTwoNumbers.java
+++ src/Implementation/_1330_CompareTwoNumbers.java
@@ -1,12 +1,12 @@
-package _001;
+package Implementation;
 
 // 26.03.17
 // 백준 1330
-// 두 수 비교하기
+// 두 수 비교하는 프로그램
 
 import java.util.Scanner;
 
-public class _013 {
+public class _1330_CompareTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
