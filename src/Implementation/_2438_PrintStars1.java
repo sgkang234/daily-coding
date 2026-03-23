baac0d206253b740580b74e8d2622da9acbe2667
sgkang234 <sgkang234@gmail.com>
2026-03-23 14:30:23 +0900

    _2438_PrintStars1

--- src/Implementation/_2438_PrintStars1.java
+++ src/Implementation/_2438_PrintStars1.java
@@ -11,7 +11,7 @@ public class _2438_PrintStars1 {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
         for (int i = 0; i < num; i++) {
-            for (int j = 0; j <i ; j++) {
+            for (int j = 0; j <=i ; j++) {
                 System.out.print("*");
             }
             System.out.println();
