f6e5caf5aff23610d8fde085a506d496994b1c72
sgkang234 <sgkang234@gmail.com>
2026-03-23 14:06:38 +0900

    _11021_APlusB_7

--- src/Implementation/_11021_APlusB_7.java
+++ src/Implementation/_11021_APlusB_7.java
@@ -1,7 +1,7 @@
-package _001;
+package Implementation;
 
-// 2026.03.20
-// 백준 15552
+// 2026.03.22
+// 백준 11021
 // 두 정수 A, B를 입력받아 Case #x: "를 출력한 다음, A+B를 출력하는 프로그램
 
 import java.io.BufferedReader;
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class _026 {
+public class _11021_APlusB_7 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
