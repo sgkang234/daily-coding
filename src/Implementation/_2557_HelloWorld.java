91e37b577431745a31ee7134808b5681d4426774
sgkang234 <sgkang234@gmail.com>
2026-03-25 21:07:13 +0900

    _2557_HelloWorld

--- src/Implementation/_2557_HelloWorld.java
+++ src/Implementation/_2557_HelloWorld.java
@@ -2,7 +2,7 @@ package Implementation;
 
 // 26.03.11
 // 백준 2557번
-//  "Hello World!" 출력하는 프로그램
+// "Hello World!" 출력하는 프로그램
 
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -10,11 +10,9 @@ import java.io.OutputStreamWriter;
 
 public class _2557_HelloWorld {
     public static void main(String[] args) throws IOException {
-
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         bw.write("Hello World!");
         bw.flush();
         bw.close();
-        //System.out.println("Hello World!");
     }
 }
