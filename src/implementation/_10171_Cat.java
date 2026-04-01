ee09d6b620fcaa164b9f2546a2e70604ae88ae6f
sgkang234 <sgkang234@gmail.com>
2026-04-02 00:37:08 +0900

    _10171_Cat

--- src/implementation/_10171_Cat.java
+++ src/implementation/_10171_Cat.java
@@ -1,12 +1,23 @@
-package Implementation;
+package implementation;
 
 // 26.03.17
 // 백준 10171
 // 고양이 출력하는 프로그램
 
+/*
+[입력]
+
+[출력]
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+*/
 public class _10171_Cat {
     public static void main(String[] args) {
-        System.out.println("\\    /\\"); // "\" 문자를 출력하려면 이스케이프 문자 때문이기에 \\ 로 작성해야 함
+        // 출력
+        // "\" 문자를 출력하려면 이스케이프 문자 때문이기에 \\ 로 작성해야 함
+        System.out.println("\\    /\\");
         System.out.println(" )  ( ')");
         System.out.println("(  /  )");
         System.out.println(" \\(__)|");
