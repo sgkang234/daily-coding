b13f22ac4e18863324766d72fe7b60d182b3f090
sgkang234 <sgkang234@gmail.com>
2026-04-02 00:38:42 +0900

    _10172_Dog

--- src/implementation/_10172_Dog.java
+++ src/implementation/_10172_Dog.java
@@ -1,14 +1,27 @@
-package Implementation;
+package implementation;
 
 // 26.03.17
 // 백준 10172
 // 강아지 출력하는 프로그램
 
+/*
+[입력]
+
+[출력]
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+*/
+
 public class _10172_Dog {
     public static void main(String[] args) {
+        // 출력
+        // " 문자를 출력하려면 이스케이프 문자 때문이기에 \" 로 작성해야 함
         System.out.println("|\\_/|");
         System.out.println("|q p|   /}");
-        System.out.println("( 0 )\"\"\"\\"); // """ 문자를 출력하려면 이스케이프 문자 때문이기에 \" 로 작성해야 함
+        System.out.println("( 0 )\"\"\"\\");
         System.out.println("|\"^\"`    |");
         System.out.println("||_/=\\\\__|");
     }
