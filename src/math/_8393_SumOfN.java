da73bf921fee3c5dde5d0d5f7edf0f586dcf4bdc
sgkang234 <sgkang234@gmail.com>
2026-04-02 01:15:56 +0900

    _8393_SumOfN

--- src/math/_8393_SumOfN.java
+++ src/math/_8393_SumOfN.java
@@ -1,8 +1,16 @@
-package Math;
+package math;
 
 // 2026.03.20
 // 백준 8393
 // 1부터 n까지 합을 구하는 프로그램
+
+/*
+[입력]
+3
+
+[출력]
+6
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,12 +18,17 @@ import java.io.InputStreamReader;
 
 public class _8393_SumOfN {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+
+        // 로직
         int sum = 0;
         for (int i = 1; i <= n; i++) {
             sum+= i;
         }
+
+        // 출력
         System.out.println(sum);
     }
 }
