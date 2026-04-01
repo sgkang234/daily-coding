957eb7118539ebe872084a92628b068cec527b39
sgkang234 <sgkang234@gmail.com>
2026-04-02 00:17:27 +0900

    _18108_BuddhistYear

--- src/implementation/_18108_BuddhistYear.java
+++ src/implementation/_18108_BuddhistYear.java
@@ -1,8 +1,16 @@
-package Implementation;
+package implementation;
 
 // 26.03.16
-// 백준 18108번
+// 백준 18108
 // 불기 입력 시 서기로 바꾸는 프로그램
+
+/*
+[입력]
+2541
+
+[출력]
+1998
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,8 +18,14 @@ import java.io.InputStreamReader;
 
 public class _18108_BuddhistYear {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int year = Integer.parseInt(br.readLine());
-        System.out.println(year - 543);
+
+        // 로직
+        int buddhistYear = year - 543;
+
+        // 출력
+        System.out.println(buddhistYear);
     }
 }
