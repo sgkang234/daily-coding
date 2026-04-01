6d140e1047bc99fde0e687f56424eb450c341025
sgkang234 <sgkang234@gmail.com>
2026-04-02 01:18:57 +0900

    _25304_Receipt

--- src/implementation/_25304_Receipt.java
+++ src/implementation/_25304_Receipt.java
@@ -1,26 +1,44 @@
-package Implementation;
+package implementation;
 
 // 2026.03.20
 // 백준 25304
 // 구매한 총 액의 가격과 종류를 입력한다. 이후 구매한 물건의 값과 개수를 입력한다. 맞으면 YES, 틀리면 NO를 출력하는 프로그램
 
+/*
+[입력]
+260000
+4
+20000 5
+30000 2
+10000 6
+5000 8
+
+[출력]
+Yes
+*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class _25304_Receipt {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int x = Integer.parseInt(br.readLine());
         int n = Integer.parseInt(br.readLine());
-        int sum = 0;
 
+        // 로직
+        int sum = 0;
         for (int i = 0; i < n; i++) {
-            String[] input = br.readLine().split(" ");
-            int price = Integer.parseInt(input[0]);
-            int count = Integer.parseInt(input[1]);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int price = Integer.parseInt(st.nextToken());
+            int count = Integer.parseInt(st.nextToken());
             sum += price * count;
         }
+
+        // 출력
         if (sum == x) { // 프로그램으로 계산된 총액과 사용자가 입력한 총액의 값이 같다면
             System.out.println("Yes");
         } else {
