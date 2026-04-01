2105f1743a72d33bfdc2e218a3c4a0293ce833fb
sgkang234 <sgkang234@gmail.com>
2026-04-02 00:44:21 +0900

    _9498_ExamGrade

--- src/implementation/_9498_ExamGrade.java
+++ src/implementation/_9498_ExamGrade.java
@@ -1,8 +1,16 @@
-package Implementation;
+package implementation;
 
 // 26.03.17
 // 백준 9498
 // 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램
+
+/*
+[입력]
+100
+
+[출력]
+A
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,9 +18,11 @@ import java.io.InputStreamReader;
 
 public class _9498_ExamGrade {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int score = Integer.parseInt(br.readLine());
 
+        // 로직 및 출력
         if (score >= 90) {
             System.out.println("A");
         } else if (score >= 80) {
