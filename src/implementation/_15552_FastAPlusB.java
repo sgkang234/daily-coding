07e1869f28ed730ca6bdca42e591e385e04f7808
sgkang234 <sgkang234@gmail.com>
2026-04-02 01:24:28 +0900

    _15552_FastAPlusB

--- src/implementation/_15552_FastAPlusB.java
+++ src/implementation/_15552_FastAPlusB.java
@@ -1,8 +1,22 @@
-package Implementation;
+package implementation;
 
 // 2026.03.20
 // 백준 15552
 // StringBuilder를 사용해 더 빠르게 처리하는 프로그램
+
+/*
+[입력]
+3
+1 1
+12 34
+5 500
+
+
+[출력]
+2
+46
+505
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,18 +25,21 @@ import java.util.StringTokenizer;
 
 public class _15552_FastAPlusB {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         int t = Integer.parseInt(br.readLine());
 
+        // 로직
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < t; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            sb.append(a+b).append('\n'); // StringBuilder를 통해 계속 입력을 쌓고 이를 한번에 출력해 빠른 처리가 가능하다
+            sb.append(a+b).append('\n');
         }
+
+        // 출력
         System.out.println(sb);
     }
 }
