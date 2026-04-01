08fab402abdfb2176b9757e5dca5332ab2455311
sgkang234 <sgkang234@gmail.com>
2026-04-02 00:13:11 +0900

    _10926_Surprised

--- src/implementation/_10926_Surprised.java
+++ src/implementation/_10926_Surprised.java
@@ -1,8 +1,16 @@
-package Implementation;
+package implementation;
 
 // 26.03.15
-// 백준 10926번
+// 백준 10926
 // 입력한 문자에 "??!"를 붙여 출력하는 프로그램
+
+/*
+[입력]
+joonas
+
+[출력]
+joonas??!
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,8 +18,11 @@ import java.io.InputStreamReader;
 
 public class _10926_Surprised {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
+
+        // 출력
         System.out.println(input + "??!");
     }
 }
