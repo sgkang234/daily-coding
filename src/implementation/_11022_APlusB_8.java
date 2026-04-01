f17b6e8e341ca907ac4cb4a69ab4bd9e568aa98f
sgkang234 <sgkang234@gmail.com>
2026-04-02 01:31:30 +0900

    _11022_APlusB_8

--- src/implementation/_11022_APlusB_8.java
+++ src/implementation/_11022_APlusB_8.java
@@ -1,8 +1,19 @@
-package Implementation;
+package implementation;
 
 // 2026.03.22
 // 백준 11022
 // 두 정수 A와 B를 입력받은 다음, 각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력하는 프로그램
+
+/*
+[입력]
+2
+1 1
+2 3
+
+[출력]
+Case #1: 1 + 1 = 2
+Case #2: 2 + 3 = 5
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,17 +22,26 @@ import java.util.StringTokenizer;
 
 public class _11022_APlusB_8 {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int num = Integer.parseInt(br.readLine());
 
+        // 로직
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= num; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
 
-            System.out.println("Case #" + i + ": " + A + " + " + B + " = " + (A + B));
+            sb.append("Case #").append(i)
+                    .append(": ")
+                    .append(A).append(" + ").append(B)
+                    .append(" = ")
+                    .append(A + B)
+                    .append('\n');
         }
+
+        // 출력
+        System.out.println(sb);
     }
 }
