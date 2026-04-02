f4319080470bf7e0bcd3a9c86222670854b24595
sgkang234 <sgkang234@gmail.com>
2026-04-02 10:34:02 +0900

    _10871_LessThanX

--- src/implementation/_10871_LessThanX.java
+++ src/implementation/_10871_LessThanX.java
@@ -1,8 +1,17 @@
-package Implementation;
+package implementation;
 
 // 26.03.23
 // 백준 10871번
 // 정수 N개로 이루어진 수열 A와 정수 X가 주어질 때 A에서 X보다 작은 수를 모두 출력하는 프로그램
+
+/*
+[입력]
+10 5
+1 10 4 9 2 3 8 5 7 6
+
+[출력]
+1 4 2 3
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,6 +20,7 @@ import java.util.StringTokenizer;
 
 public class _10871_LessThanX {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
@@ -18,12 +28,15 @@ public class _10871_LessThanX {
         int x = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
 
+        // 로직
         for (int i = 0; i < n; i++) {
             int a = Integer.parseInt(st.nextToken());
             if(a < x){
                 sb.append(a).append(" ");
             }
         }
+
+        // 출력
         System.out.println(sb);
     }
 }
