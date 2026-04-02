0a6f1ac2fb1bb8be2d7eeb024b9de8c7abeb32ab
sgkang234 <sgkang234@gmail.com>
2026-04-02 11:28:12 +0900

    _2908_ReverseNumber

--- src/strings/_2908_ReverseNumber.java
+++ src/strings/_2908_ReverseNumber.java
@@ -1,8 +1,16 @@
-package String;
+package strings;
 
 // 26.03.27
 // 백준 2908
 // 두 수를 입력하면 (734, 893) 이를 (437, 398) 역수 생각해 두 수 중 더 큰 수를 입력하는 프로그램
+
+/*
+[입력]
+734 893
+
+[출력]
+437
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,12 +19,15 @@ import java.util.StringTokenizer;
 
 public class _2908_ReverseNumber {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        // 로직
         String a = new StringBuilder(st.nextToken()).reverse().toString();
         String b = new StringBuilder(st.nextToken()).reverse().toString();
 
+        // 출력
         System.out.println(Math.max(Integer.parseInt(a), Integer.parseInt(b)));
     }
 }
