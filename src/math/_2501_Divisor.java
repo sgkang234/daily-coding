180426bb0eccb2e3ac06f44d9a5ff6e34a9adfea
sgkang234 <sgkang234@gmail.com>
2026-04-02 23:01:48 +0900

    _2501_Divisor

--- src/math/_2501_Divisor.java
+++ src/math/_2501_Divisor.java
@@ -1,8 +1,16 @@
-package Math;
+package math;
 
 // 26.03.31
 // 백준 2501
 // 두 개의 자연수 N과 K가 주어졌을 때, N의 약수들 중 K번째로 작은 수를 출력하는 프로그램
+
+/*
+[입력]
+6 3
+
+[출력]
+3
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,12 +19,15 @@ import java.util.StringTokenizer;
 
 public class _2501_Divisor {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int count = 0;
 
+        // 로직
+        int count = 0;
+        // 0으로 나누면 안되고, 자기 자신까지 약분하는 반복문
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 count++;
@@ -26,6 +37,8 @@ public class _2501_Divisor {
                 }
             }
         }
+
+        // 출력
         System.out.println(0);
     }
 }
