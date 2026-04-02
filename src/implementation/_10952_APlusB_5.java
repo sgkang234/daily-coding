eb89d4bca4ed5efeb35ced8ad123f11074dc99b2
sgkang234 <sgkang234@gmail.com>
2026-04-02 10:19:12 +0900

    _10952_APlusB_5

--- src/implementation/_10952_APlusB_5.java
+++ src/implementation/_10952_APlusB_5.java
@@ -1,8 +1,21 @@
-package Implementation;
+package implementation;
 
 // 26.03.23
 // 백준 10952
 // 정수 A, B를 입력받아 A와 B가 모두 0이 아닐 때까지 A+B를 출력하는 프로그램
+
+/*
+[입력]
+1 1
+2 3
+3 4
+0 0
+
+[출력]
+2
+5
+7
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,9 +24,11 @@ import java.util.StringTokenizer;
 
 public class _10952_APlusB_5 {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
+        // 로직
         while (true){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -24,6 +39,8 @@ public class _10952_APlusB_5 {
             }
             sb.append(a+b).append("\n");
         }
+
+        // 출력
         System.out.println(sb);
     }
 }
