f60a1f350a6579c6164843a7b2161da097d28561
sgkang234 <sgkang234@gmail.com>
2026-04-02 11:18:38 +0900

    _11720_SumOfDigits

--- src/strings/_11720_SumOfDigits.java
+++ src/strings/_11720_SumOfDigits.java
@@ -1,8 +1,18 @@
-package String;
+package strings;
 
 // 26.03.26
 // 백준 11720
 // 첫째 줄에 숫자의 개수 정수 N이 주어지고, 둘째 줄부터 숫자 N개가 공백 없이 주어질 때 이 숫자를 모두 합해서 출력하는 프로그램
+
+/*
+[입력]
+5
+54321
+
+[출력]
+15
+*/
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,13 +20,18 @@ import java.io.InputStreamReader;
 
 public class _11720_SumOfDigits {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int i = Integer.parseInt(br.readLine());
         String s = br.readLine();
+
+        // 로직
         int sum = 0;
         for (int j = 0; j < i; j++) {
             sum += s.charAt(j) - '0'; // char끼리 연산하여 자동으로 int로 형변환
         }
+
+        // 출력
         System.out.println(sum);
     }
 }
