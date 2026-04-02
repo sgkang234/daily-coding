6b001ad5814964eb636c51636a3fb3dced67768b
sgkang234 <sgkang234@gmail.com>
2026-04-02 11:16:10 +0900

    _11654_ASCII

--- src/strings/_11654_ASCII.java
+++ src/strings/_11654_ASCII.java
@@ -1,8 +1,16 @@
-package Implementation;
+package strings;
 
 // 26.03.26
 // 백준 11654
 // 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램
+
+/*
+[입력]
+A
+
+[출력]
+65
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,9 +18,11 @@ import java.io.InputStreamReader;
 
 public class _11654_ASCII {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char c = br.readLine().charAt(0);
-        System.out.println((int)c);
 
+        // 출력
+        System.out.println((int)c);
     }
 }
