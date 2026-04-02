e912495a289672226e2e3c1e2c15d1b4ca0ef086
sgkang234 <sgkang234@gmail.com>
2026-04-02 11:09:27 +0900

    _27866_CharacterAndString

--- src/strings/_27866_CharacterAndString.java
+++ src/strings/_27866_CharacterAndString.java
@@ -1,8 +1,17 @@
-package String;
+package strings;
 
 // 26.03.26
 // 백준 27866
 // 영어 문자열 S와 정수 i가 주어졌을 때, S의 i번째 글자를 출력하는 프로그램
+
+/*
+[입력]
+Sprout
+3
+
+[출력]
+r
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,10 +19,12 @@ import java.io.InputStreamReader;
 
 public class _27866_CharacterAndString {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         int i = Integer.parseInt(br.readLine());
 
+        // 출력
         System.out.println(s.charAt(i-1)); // 문자열에서 특정 위치의 문자 하나를 가져오는 메서드
     }
 }
