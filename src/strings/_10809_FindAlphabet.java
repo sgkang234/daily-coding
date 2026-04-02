a338fbcfec00db7e4d01ff68dbb114eb1921fb2f
sgkang234 <sgkang234@gmail.com>
2026-04-02 11:22:38 +0900

    _10809_FindAlphabet

--- src/strings/_10809_FindAlphabet.java
+++ src/strings/_10809_FindAlphabet.java
@@ -1,8 +1,16 @@
-package String;
+package strings;
 
 // 26.03.27
 // 백준 10809
 // 알파벳 소문자로만 이루어진 단어 S가 주어질 때, 각 알파벳에 대해 단어에 포함되어 있는 경우에는 처음 등장하는 위치를, 포함되어 있지 않은 경우에는 -1을 출력하는 프로그램
+
+/*
+[입력]
+baekjoon
+
+[출력]
+1 0 -1 -1 2 -1 -1 -1 -1 4 3 -1 -1 7 5 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,18 +18,17 @@ import java.io.InputStreamReader;
 
 public class _10809_FindAlphabet {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         String s = br.readLine();
 
-        // 알파벳 배열
-        int[] arr = new int[26];
-
+        // 로직
+        int[] arr = new int[26]; // 알파벳 배열
         // 모든 배열 -1 초기화
         for (int i = 0; i < 26; i++) {
             arr[i] = -1;
         }
-
         //  char 데이터 타입을 형변환을 통해 숫자로 형변환 이를 통해 a는 0
         for (int i = 0; i < s.length(); i++) {
             int index = s.charAt(i) - 'a';
@@ -32,6 +39,8 @@ public class _10809_FindAlphabet {
         for (int i = 0; i < 26; i++) {
             sb.append(arr[i]).append(" ");
         }
+
+        // 출력
         System.out.println(sb);
     }
 }
