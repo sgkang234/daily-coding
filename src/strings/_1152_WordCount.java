4a7e8af7f6639d0603aef1a85e42c3aaa3081fa8
sgkang234 <sgkang234@gmail.com>
2026-04-02 11:25:56 +0900

    _1152_WordCount

--- src/strings/_1152_WordCount.java
+++ src/strings/_1152_WordCount.java
@@ -1,8 +1,16 @@
-package String;
+package strings;
 
 // 26.03.27
 // 백준 1152
 // 문자열로 된 알파벳 단어가 몇번 등장하는지 횟수를 출력하는 프로그램
+
+/*
+[입력]
+The Curious Case of Benjamin Button
+
+[출력]
+6
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,8 +19,10 @@ import java.util.StringTokenizer;
 
 public class _1152_WordCount {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()); // 공백 자동 처리
+        // 출력
         System.out.println(st.countTokens());
     }
 }
