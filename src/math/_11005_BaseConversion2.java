c8f8e003ecd975e6d4824ed04d3bfb5c67b261e1
sgkang234 <sgkang234@gmail.com>
2026-04-02 16:31:01 +0900

    _11005_BaseConversion2

--- src/math/_11005_BaseConversion2.java
+++ src/math/_11005_BaseConversion2.java
@@ -1,8 +1,16 @@
-package Math;
+package math;
 
 // 26.03.30
 // 백준 11005
 // 진법 전환 프로그램2
+
+/*
+[입력]
+60466175 36
+
+[출력]
+ZZZZZ
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,12 +19,13 @@ import java.util.StringTokenizer;
 
 public class _11005_BaseConversion2 {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
+        // 로직
         StringBuilder sb = new StringBuilder();
         while (n > 0) {
             int r = n % b;
@@ -28,6 +37,8 @@ public class _11005_BaseConversion2 {
             }
             n /= b;
         }
+
+        // 출력
         System.out.println(sb.reverse());
     }
 }
