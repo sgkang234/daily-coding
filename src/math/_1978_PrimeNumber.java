53bcfa7ab1f28bf908e245098b2c70582a92ac1c
sgkang234 <sgkang234@gmail.com>
2026-04-02 23:05:23 +0900

    _1978_PrimeNumber

--- src/math/_1978_PrimeNumber.java
+++ src/math/_1978_PrimeNumber.java
@@ -1,8 +1,17 @@
-package Math;
+package math;
 
 // 26.04.01
 // 백준 1978
 // 주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램
+
+/*
+[입력]
+4
+1 3 5 7
+
+[출력]
+3
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,11 +20,13 @@ import java.util.StringTokenizer;
 
 public class _1978_PrimeNumber {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int count = 0; // 소수의 개수
 
+        // 로직
+        int count = 0; // 소수의 개수
         // 소수인지 아닌지 확인하는 반복문
         for (int i = 0; i < num; i++) {
             int prime = Integer.parseInt(st.nextToken());
@@ -29,6 +40,8 @@ public class _1978_PrimeNumber {
             }
             if (isPrime) count++;
         }
+
+        // 출력
         System.out.println(count);
     }
 }
