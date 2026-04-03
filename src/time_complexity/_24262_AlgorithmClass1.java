be90df91f789460003f8614e6392ff3932bc0045
sgkang234 <sgkang234@gmail.com>
2026-04-03 11:06:27 +0900

    _24262_AlgorithmClass1

---
+++ src/time_complexity/_24262_AlgorithmClass1.java
@@ -0,0 +1,24 @@
+package time_complexity;
+
+// 26.04.03
+// 백준 24262
+// return A[n / 2]; 의 시간 복잡도 개념 확인 프로그램
+/*
+return A[n / 2];는 배열의 특정 인덱스에 한 번만 접근하므로, 입력 크기 n과 관계없이 실행 횟수가 일정하다.
+따라서 시간 복잡도는 O(1)이다.
+*/
+
+/*
+[입력]
+
+[출력]
+1
+0
+*/
+
+public class _24262_AlgorithmClass1 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
